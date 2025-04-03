4c886729536b79408208abd2e0761c27d81c4f2e
dmitryzam <dmitryzam@mail.ru>
2025-04-03 17:50:35 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void WriteToFile(string path, string content)
+        {
+            StreamWriter sr = new StreamWriter(path, true);
+            sr.Write(content);
+            sr.Close();
+        }
+        static void Main(string[] args)
+        {
+            File.WriteAllText("out.txt", string.Empty);
+            int num = int.Parse(args[0]);
+            string result = string.Empty;
+            int multy = 1;
+            if(args.Length >= 1)
+            {
+                if (int.Parse(args[0]) > 1) 
+                {
+                    for(int i = 1; i < num + 1; i++)
+                    {
+                        if(i == 1)
+                        {
+                            for(int j = 1; j < num + 1; j++)
+                            {
+                                if(j == 1)
+                                {
+                                    result = "\t";
+                                    WriteToFile("out.txt", result);
+                                }
+                                if(j > 1)
+                                {
+                                    result = $"{j}\t";
+                                    WriteToFile("out.txt", result);
+                                }
+                                if(j == num)
+                                {
+                                    result = "\n";
+                                    WriteToFile("out.txt", result);
+                                }
+                            }
+                        }
+                        if(i > 1)
+                        {
+                            for (int j = 1; j < num + 1; j++)
+                            {
+                                if (j == 1)
+                                {
+                                    result = $"{i}\t";
+                                    WriteToFile("out.txt", result);
+                                }
+                                if (j > 1)
+                                {
+                                    multy = i * j;
+                                    result = $"{multy}\t";
+                                    WriteToFile("out.txt", result);
+                                }
+                                if (j == num)
+                                {
+                                    result = "\n";
+                                    WriteToFile("out.txt", result);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Программа работает");
+            }
+        }
+    }
+}
